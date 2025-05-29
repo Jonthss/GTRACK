e8c7cc233922330cd58e271c0c6b105afe9a28c3
Jonthss <jonathas9000@gmail.com>
2025-05-29 18:04:04 -0300

    Refactor GameItem and GameList components for improved structure and styling

--- src/components/game/GameList.jsx
+++ src/components/game/GameList.jsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 import GameItem from './GameItem';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const GameList = ( games, sliceValue = games.length) => {
+const GameList = ({ games, sliceValue = games.length }) => {
+  // by default we are going to display all the games fetched from api if not any slicevalue is mentioned
   return (
     <GameListWrapper>
       <div className='card-list'>
         {
-          games.slice(0, sliceValue).map((game) => (
-            <GameItem key = { GameItem.id } gameItem = { item } />
+          games?.slice(0, sliceValue).map(item => (
+            <GameItem key = { item.id } gameItem = { item } />
           ))
         }
       </div>
@@ -20,7 +21,7 @@ export default GameList;
 
 GameList.propTypes = {
   games: PropTypes.array,
-  sliceValue: PropTypes.number,
-};
+  sliceValue: PropTypes.number
+}
 
 const GameListWrapper = styled.div``;
